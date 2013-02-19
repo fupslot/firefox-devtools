fcc8c6e35c9dba3a2c2b567c7ed037d49da88c56
Eugene Brodsky <fupslot@gmail.com>
2013-02-19 10:14:27 +0200

    main.css main.js

--- src/chrome/content/inspector/main.js
+++ src/chrome/content/inspector/main.js
@@ -1,6 +1,34 @@
 (function () {
 
 
-	alert("Got Ya!");
+function DIOnMouseOver(evt)
+{
+    element = evt.target;   // not IE
+
+    // set the border around the element
+    element.style.borderWidth = '2px';
+    element.style.borderStyle = 'solid';
+    element.style.borderColor = '#f00';
+}
+
+
+function DIOnMouseOut(evt)
+{
+    evt.target.style.borderStyle = 'none';
+}
+
+
+function DIOnClick(evt)
+{
+    var selection = evt.target.innerHTML;
+
+    alert('Element is: ' + evt.target.toString() + '\n\nSelection is:\n\n' + selection);
+    return false;
+}
+
+
+document.addEventListener("mouseover", DIOnMouseOver, true);
+document.addEventListener("mouseout", DIOnMouseOut, true);
+document.addEventListener("click", DIOnClick, true);
 
 })();
