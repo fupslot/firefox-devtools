6e0199f2637aae65e95fe8b5f9dc6c5559123898
Eugene Brodsky <fupslot@gmail.com>
2013-02-19 11:18:14 +0200

    main.js

--- src/chrome/content/inspector/main.js
+++ src/chrome/content/inspector/main.js
@@ -1,34 +1,35 @@
 (function () {
 
+			
+	function OnMouseOver(e) {
+	  element = e.target;   // not IE
 
-function DIOnMouseOver(evt)
-{
-    element = evt.target;   // not IE
+	  // set the border around the element
+	  element.style.borderWidth = '2px';
+	  element.style.borderStyle = 'solid';
+	  element.style.borderColor = '#f00';
+	}
 
-    // set the border around the element
-    element.style.borderWidth = '2px';
-    element.style.borderStyle = 'solid';
-    element.style.borderColor = '#f00';
-}
-
-
-function DIOnMouseOut(evt)
-{
+ 
+	function OnMouseOut(evt) {
     evt.target.style.borderStyle = 'none';
-}
+	}
 
+	function OnClick(e) {
+		cancelEvent(e);
+	  var selection = e.target.innerHTML;
 
-function DIOnClick(evt)
-{
-    var selection = evt.target.innerHTML;
+	  alert('Element is: ' + e.target.toString() + '\n\nSelection is:\n\n' + selection);
+	  return false;
+	}
 
-    alert('Element is: ' + evt.target.toString() + '\n\nSelection is:\n\n' + selection);
-    return false;
-}
+	function cancelEvent (e) {
+		e.preventDefault();
+		e.stopImmediatePropagation();
+		e.stopPropagation();
+	}
 
-
-document.addEventListener("mouseover", DIOnMouseOver, true);
-document.addEventListener("mouseout", DIOnMouseOut, true);
-document.addEventListener("click", DIOnClick, true);
-
+	document.addEventListener("mouseover", OnMouseOver, true);
+	document.addEventListener("mouseout", OnMouseOut, true);
+	document.addEventListener("click", OnClick, true);
 })();
